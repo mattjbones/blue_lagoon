744cc433dc58b9cf7887d9272dae272355969810
mattjbones <mattjbones@gmail.com>
2020-04-25 11:39:54 +0100

    starting projecct with webpack, webpack-dev-server, and mythril added

---
+++ app/src/index.js
@@ -0,0 +1,2 @@
+import m from "mithril";
+m.render(document.getElementById('app'), "Blu");
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./app/src/index.js",
+  output: {
+	 path: path.resolve(__dirname, 'dist'),
+	 filename: '[name].[hash].bundle.js',
+  }, 
+	plugins: [
+    new HtmlWebpackPlugin({
+      hash: true, 
+      filename: 'dist/index.html',
+      template: 'app/index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+	],
+  watch: true
+};
